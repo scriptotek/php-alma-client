4b4a4f3a32b13b6a3dc90ade78c100f32043c628
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-08-21 11:17:53 +0200

    tests: improve Bibs spec

--- spec/BibsSpec.php
+++ spec/BibsSpec.php
@@ -8,23 +8,20 @@ use Scriptotek\Alma\Models\Bib;
 use Scriptotek\Alma\Client as AlmaClient;
 use Scriptotek\Alma\Factory;
 use Danmichaelo\QuiteSimpleXMLElement\QuiteSimpleXMLElement;
-
+use Scriptotek\Sru\Client as SruClient;
+use Scriptotek\Sru\Record as SruRecord;
 
 class BibsSpec extends ObjectBehavior
 {
-
-    public function let(AlmaClient $almaClient)
+    public function it_is_initializable(AlmaClient $almaClient)
     {
         $this->beConstructedWith($almaClient);
-    }
-
-    function it_is_initializable()
-    {
         $this->shouldHaveType('Scriptotek\Alma\Bibs');
     }
 
-    public function it_provides_an_array_interface_to_bib_objects(AlmaClient $almaClient, Bib $bib)
+    public function it_provides_an_array_interface_to_bib_objects(AlmaClient $almaClient)
     {
+        $this->beConstructedWith($almaClient);
         $almaClient->getXML('/bibs/123')
             ->shouldBeCalled()
             ->willReturn(new QuiteSimpleXMLElement('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><bib><mms_id>123</mms_id><record><leader>02615cam a22002417u 4500</leader></record></bib>'));
@@ -37,4 +34,42 @@ class BibsSpec extends ObjectBehavior
         $bib->mms_id->shouldBe($mms_id);
     }
 
+    public function it_returns_a_bib_object_given_an_isbn(AlmaClient $almaClient, SruClient $sru)
+    {
+        $this->beConstructedWith($almaClient);
+        $almaClient->sru = $sru;
+
+        $sru->first('alma.isbn="123"')
+            ->shouldBeCalled()
+            ->willReturn(SruRecord::make(1,
+                '<record><controlfield tag="001">990114012304702201</controlfield></record>'
+                ));
+
+        $almaClient->getXML('/bibs/990114012304702201')
+            ->shouldBeCalled()
+            ->willReturn(QuiteSimpleXMLElement::make(
+                '<bib><mms_id>990114012304702201</mms_id><record><leader>02615cam a22002417u 4500</leader></record></bib>'
+                ));
+
+        $bib = $this->fromIsbn('123');
+        $bib->shouldHaveType('Scriptotek\Alma\Models\Bib');
+        $bib->mms_id->shouldBe('990114012304702201');
+    }
+
+    public function it_returns_null_given_unknown_isbn(AlmaClient $almaClient, SruClient $sru)
+    {
+        $this->beConstructedWith($almaClient);
+        $almaClient->sru = $sru;
+
+        $sru->first('alma.isbn="123"')
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $bib = $this->fromIsbn('123');
+        $bib->shouldBe(null);
+    }
+
+    public function it_returns_a_bib_object_given_a_barcode(AlmaClient $almaClient)
+    {
+    }
 }
--- spec/models/BibSpec.php
+++ spec/models/BibSpec.php
@@ -2,42 +2,20 @@
 
 namespace spec\Scriptotek\Alma\models;
 
-use Danmichaelo\QuiteSimpleXMLElement\QuiteSimpleXMLElement;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Scriptotek\Alma\Client as AlmaClient;
+use spec\Scriptotek\Alma\SpecHelper;
 
 class BibSpec extends ObjectBehavior
 {
-
-    public $sample = '<bib>
-          <mms_id>abc123</mms_id>
-          <holdings link="/almaws/v1/bibs/991122800000121/holdings"/>
-          <created_by>exl_impl</created_by>
-          <created_date>2013-11-05Z</created_date>
-          <last_modified_by>exl_impl</last_modified_by>
-          <last_modified_date>2014-01-20Z</last_modified_date>
-          <record>
-            <leader>00260nam a2200109 u 4500</leader>
-            <controlfield tag="001">991122800000121</controlfield>
-            <controlfield tag="005">20140120122820.0</controlfield>
-            <controlfield tag="008">131105s2013 xx r 000 0 gsw d</controlfield>
-            <datafield ind1="1" ind2=" " tag="100">
-              <subfield code="a">Smith, John</subfield>
-            </datafield>
-            <datafield ind1="1" ind2="0" tag="245">
-              <subfield code="a">Book of books</subfield>
-            </datafield>
-          </record>
-        </bib>';
-
     public function let(AlmaClient $almaClient)
     {
-        $mms_id = 'abc123';
+        $mms_id = '990114012304702204';
         $this->beConstructedWith($mms_id, $almaClient);
-        $almaClient->getXML(Argument::containingString('abc123'))
+        $almaClient->getXML(Argument::containingString('990114012304702204'))
             ->shouldBeCalled()
-            ->willReturn(new QuiteSimpleXMLElement($this->sample));
+            ->willReturn(SpecHelper::getDummyData('bib_response.xml'));
         $this->fetch();
     }
 
@@ -53,6 +31,6 @@ class BibSpec extends ObjectBehavior
 
     public function it_should_provide_data(AlmaClient $almaClient)
     {
-        $this->created_date->shouldBe('2013-11-05Z');
+        $this->created_date->shouldBe('2015-02-10Z');
     }
 }
