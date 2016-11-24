01e874b3ea1b111221e18fd95f58bdfff1e87c1a
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-11-24 22:57:16 +0100

    test: Add User test data

--- spec/Users/UserSpec.php
+++ spec/Users/UserSpec.php
@@ -14,6 +14,9 @@ class UserSpec extends ObjectBehavior
     public function let(AlmaClient $almaClient)
     {
         $this->beConstructedWith($almaClient, '123456', []);
+
+        $almaClient->getJSON(Argument::containingString('12345'), Argument::any())
+            ->willReturn(SpecHelper::getDummyData('user_response.json'));
     }
 
     public function it_is_initializable()
@@ -21,13 +24,11 @@ class UserSpec extends ObjectBehavior
         $this->shouldHaveType(User::class);
     }
 
-    public function it_fetches_data_only_when_needed(AlmaClient $almaClient)
+    public function it_has_identifiers()
     {
-        $almaClient->getJSON(Argument::containingString('12345'), Argument::any())
-            ->shouldBeCalled()
-            ->willReturn(SpecHelper::getDummyData('user_response.json'));
-
         $this->primary_id->shouldBe('12345');
+        $this->getUniversityId()->shouldBe('test@uio.no');
+        $this->getBarcode()->shouldBe(null);
     }
 
 }
