4676eb6b92a5771d06de0625f85518c0b910f47c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-08-03 16:37:45 +0200

    Add scan-in test

--- spec/Bibs/ItemSpec.php
+++ spec/Bibs/ItemSpec.php
@@ -81,6 +81,18 @@ class ItemSpec extends ObjectBehavior
             ->shouldHaveType(ScanInResponse::class);
     }
 
+    function it_can_be_scanned_in_with_params(AlmaClient $client, Library $library)
+    {
+        $client->postJSON('/bibs/990006312214702204/holdings/22163771200002204/items/23163771190002204?place_on_hold_shelf=true&op=scan&library=THAT+LIBRARY&circ_desk=OTHER_DESK')
+            ->shouldBeCalled()
+            ->willReturn(SpecHelper::getDummyData('scanin_transit_response.json'));
+
+        $library->code = 'THAT LIBRARY';
+
+        $this->scanIn($library, 'OTHER_DESK', ['place_on_hold_shelf' => 'true'])
+            ->shouldHaveType(ScanInResponse::class);
+    }
+
     public function it_has_requests()
     {
         $this->requests->shouldHaveType(Requests::class);
