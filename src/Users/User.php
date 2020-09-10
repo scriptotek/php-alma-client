e27da6c9611c1e9fb4206bb730ad6dbdbe9f9e0c
Clinton Graham <ctgraham@pitt.edu>
2020-09-10 11:11:53 -0400

    Enable save() method for User and enable SMS manipulation (#17)

--- src/Users/User.php
+++ src/Users/User.php
@@ -84,6 +84,89 @@ class User extends LazyResource
     }
 
     /**
+     * Get the user's preferred SMS number.
+     *
+     * @return string|null
+     */
+    public function getSmsNumber()
+    {
+        $this->init();
+        if ($this->data->contact_info->phone) {
+            foreach ($this->data->contact_info->phone as $phone) {
+                if ($phone->preferred_sms) {
+                    return $phone->phone_number;
+                }
+            }
+        }
+        return null;
+    }
+ 
+    /**
+     * Remove the preferred SMS flag from any number.
+     */
+    public function unsetSmsNumber()
+    {
+        $this->init();
+        if ($this->data->contact_info->phone) {
+            foreach ($this->data->contact_info->phone as $phone) {
+                if ($phone->preferred_sms) {
+                    $phone->preferred_sms = false;
+                }
+            }
+        }
+    }
+
+    /**
+     * Set the user's preferred SMS number, creating a new internal mobile number if needed
+     * @param $number string The SMS-capable mobile phone number
+     */
+    public function setSmsNumber($number)
+    {
+        $currentNumber = $this->getSmsNumber();
+        if ($number === $currentNumber) {
+            return;
+        }
+        $this->unsetSmsNumber();
+        $updated = false;
+        if ($this->data->contact_info->phone) {
+            foreach ($this->data->contact_info->phone as $phone) {
+                if ($phone->phone_number === $number) {
+                    $phone->preferred_sms = true;
+                }
+            }
+        }
+        if (!$updated) {
+            $this->addSmsNumber($number);
+        }
+    }
+
+    /**
+     * Add the user's preferred SMS number as a new internal mobile number.
+     * @param $number string The SMS-capable mobile phone number
+     */
+    public function addSmsNumber($number)
+    {
+        $currentNumber = $this->getSmsNumber();
+        if ($currentNumber) {
+            $this->unsetSmsNumber();
+        }
+        $phones = json_decode(json_encode($this->data->contact_info->phone), true);
+        $phones[] = json_decode('{"phone_number":'.json_encode($number).',"preferred":false,"preferred_sms":true,"segment_type":"Internal","phone_type":[{"value":"mobile","desc":"Mobile"}]}', true);
+        $this->data->contact_info->phone = json_decode(json_encode($phones));
+    }
+
+    /**
+     * Save the user
+     * 
+     * @return string The API response body
+     */
+    public function save()
+    {
+        $this->init();
+        return $this->client->put($this->url(), json_encode($this->jsonSerialize()));
+    }
+
+    /**
      * Check if we have the full representation of our data object.
      *
      * @param \stdClass $data
